6af037bbfd6d599484af140295f70ac9bd65964c
Hanif Aliffudin <68997219+hanifaliffudin@users.noreply.github.com>
2024-11-17 23:41:41 +0700

    fix login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
+  const router = useRouter();
+
   return (
     <div>
       <section className="bg-gray-50">
@@ -80,13 +85,13 @@ const LoginPage = () => {
                     Forgot password?
                   </Link>
                 </div>
-                <Link
-                  // type="submit"
-                  href="/home"
+                <button
+                  type="button"
+                  onClick={() => router.push("/home")}
                   className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
                 >
                   Sign in
-                </Link>
+                </button>
                 <p className="text-sm font-light text-gray-500 ">
                   Don&apos;t have an account yet?{" "}
                   <Link
